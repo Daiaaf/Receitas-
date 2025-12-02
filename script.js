548540be0ac3d6a1fca4e73f1b2cb561fade233b
Daiaaf <daiaaf@live.com>
2025-12-02 15:19:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const cards = document.querySelectorAll('.card');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-}, { threshold: 0.2 });
-
-cards.forEach(card => {
-    card.classList.add('fade-in');
-    observer.observe(card);
-});
-
-console.log("Tema tropical alegre carregado!");
+console.log('Site carregado com sucesso!');
